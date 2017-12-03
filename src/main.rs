266e123bd2b5790e3f6a844efdcf99340f385399
Alberto Leal <mailforalberto@gmail.com>
2017-12-03 04:37:41 -0500

    clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+// crates
+
+extern crate clap;
+extern crate csv;
+
+// 3rd-party imports
+
+use clap::{App, Arg};
+
 fn main() {
+    let matches = App::new("td-transactions-to-ledger")
+        .version("0.1.0")
+        .author("Alberto Leal <mailforalberto@gmail.com>")
+        .about("Convert TD Bank transactions to ledger compatible transactions")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true),
+        )
+        .get_matches();
+
+    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
+
     println!("Hello, world!");
 }
