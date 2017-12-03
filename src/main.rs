eed883f67df4e54c62747a933fd812f93422fcb2
Alberto Leal <mailforalberto@gmail.com>
2017-12-03 05:00:30 -0500

    initial parse

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@
 
 extern crate clap;
 extern crate csv;
+extern crate chrono;
 
 // 3rd-party imports
 
 use clap::{App, Arg};
+use chrono::NaiveDate;
 
 fn main() {
+
+    // cli args
+
     let matches = App::new("td-transactions-to-ledger")
         .version("0.1.0")
         .author("Alberto Leal <mailforalberto@gmail.com>")
@@ -19,7 +24,35 @@ fn main() {
         )
         .get_matches();
 
-    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
+    let path_to_file = matches.value_of("INPUT").unwrap();
 
-    println!("Hello, world!");
+    // csv reader
+
+    let mut reader = match csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_path(path_to_file) {
+            Ok(reader) => reader,
+            Err(err_reason) => {
+                println!("{}", err_reason);
+                std::process::exit(1);
+            }
+        };
+
+    for result in reader.records() {
+
+        let record = result.unwrap();
+
+        // CSV format: date, transaction description, positive, negative
+
+        let date = record.get(0).unwrap().trim().to_string();
+        let description = record.get(1).unwrap().trim().to_string();
+        let debit = record.get(2).unwrap().trim().to_string();
+        let credit = record.get(3).unwrap().trim().to_string();
+
+        let date = NaiveDate::parse_from_str(&date, "%m/%d/%Y").unwrap();
+
+        println!("{} {} {} {}", date.format("%Y-%m-%d"), description, debit, credit);
+
+
+    }
 }
